cfcb91e14ad26b5db371a2c4f9eb8cb03aee6dc7
B189286-2021 <email_not_provided>
2021-11-29 10:35:42 +0000

    Edits to part3 of code + bugs

--- protein.py
+++ protein.py
@@ -40,39 +40,42 @@ details["taxonomic_group"] = input("Please enter the taxonomic group: ")
 user_entry(*list(details.values()))
 
 #Esearch using Edirect
-#NB ASK USER IF THEY WANT FULL/AND PARTIAL SEQUENCES??????***************
 mycommand1="esearch -db protein -query '{0}[organism] AND {1}[Protein name] NOT PARTIAL' | efetch -format fasta > {0}_esearch.fasta ".format(details["taxonomic_group"], details["protein"])
 subprocess.call(mycommand1, shell = True)
 
 #Print statement when files are downloaded
 print("Sequences for {} protein in the {} taxonomic group are now downloaded!".format(details["protein"], details["taxonomic_group"]))
 
+
+#_______________________________________________________________________________________________#
+
+
 #Ask the user if they would like to continue and count the number of sequences were retrieved?
 count_seq=input("Would you like to proceed and count the number of sequences retrived? [Yes/No]").upper()
 
 if count_seq=="YES":
 	print("The total number of sequences in the FASTA file is: ")
-	command2='open("{}_esearch.fa".format(details["taxonomic_group"]).read()'
+	command2='open("{0}_esearch.fa".format(details["taxonomic_group"]).read()'
 	subprocess.call(command2, shell=True)
-	command3='seqcount("{}_esearch.fa".format(details["taxonomic_group])
+	command3='seqcount("{0}_esearch.fa".format(details["taxonomic_group"])
 	subprocess.call(command3, shell=True)
 
 if count_seq=="NO":
 	print("Session ended.")
 	sys.exit()
 
-#Create a list of different species - info taken from the header??????????######
+######Create a list of different species - info taken from the header??
 
-#Ask the user if they want to continue and align the sequencesa
+#Ask the user if they want to continue and align the sequences
 #sequences must first be split (seqretsplit???) and then aligned usilng clustalo
 alig_seq=("Would you like to coninue and align the sequences?[Yes/No]").upper()
 
 if align_seq=="YES":
 	print("Sequences were split and then aligned.")
-	command4='("{}_esearch.fa".format(details["taxonomic_group]).split">" > esearch_split.fa)
+	command4='("{0}_esearch.fa".format(details["taxonomic_group"]).split">" > esearch_split.fa)'
 	subprocess.call(command4, shell, True)
 	#clustalo: align multiple sequences
-	command5='clustalo -i esearch_split.fa -o prot_clust.msf -t protein --outfmt msf -v
+	command5='clustalo -i esearch_split.fa -o prot_clust.msf -t protein --outfmt msf -v'
 	subprocess.call(command5, shell=True)
 
 if align_seq=="NO":
@@ -85,11 +88,12 @@ info_align=("Would you like to create a file with information on the alignement?
 
 if info_align=="YES":
 	print("Alginment info file created")
-	command6='infoalign FILE.fa > info_align.fa'
+	command6='infoalign prot_clust.msf > info_align.fa'
 	subrocess.call(command6, shell=True)
 
 if info_alig=="NO":
-	print
+	print("Exiting programme.")
+	sys.exit()
 
 #Ask the user if they would like to create a conservation plot?
 con_plot=("Would you like to display the coservation plot?[Yes/No]").upper()
@@ -121,13 +125,13 @@ elif con_plot=="No":
 #individual files of each sequence required for this.
 ind_seqfile=("Would you like to create files for each sequence? This is required if you want to scan the sequences for known motifs. [Yes/No]")
 
-if ind_seqfile="Yes":
+if ind_seqfile=="YES":
 	print("Individual files created")
-	command9='seqretsplit FILE.fa seqoutall'
+	command9='seqretsplit {0}_esearch.fa seqoutall > {0}_esearch.seq.fa'
 	subprocess.call(command9, shell=True)
-	command10='patmatmotifs -full -sequence FILE -sformat fasta'
+	command10='patmatmotifs -full -sequence {0}_esearch.seq.fa -sformat fasta'
 	subprocess.call(command10, shell=True)
-if ind_seqfile="No":
+if ind_seqfile=="NO":
 	print("Session ended.")
         sys.exit()
 
@@ -139,7 +143,35 @@ if ind_seqfile="No":
 
 #________________________________________________________________________________#
 
-#os.environ[]
 
+#Ask user if they want to predict secondary protein structure? 
 
+pred_struc=("Would you like to predict the secondary protein structure?[Yes/No]").upper()
 
+if pre_dstruc=="YES":
+	print("File created with info on secondary protein structure")
+	command11='garnier {0}_esearch.seq.fa  > {0}.garnier'
+	subprocess.call(command11, shell=True)
+
+if pred_struc=='NO':
+	print("Exiting programe")
+	sys.exit()
+
+#Ask the user if they want to get aa summary:
+
+aa_plot=("Would you like to save or generate a aa plot?"[Yes/No]).upper()
+
+#NB note that the seq is a protein 
+if aa_plot=="YES":
+	print("Would you like to print or saven it?[P/S]).upper()
+	if P:
+		command12='pepinfo {0}_esearch.seq.fa  -sprotein1 -graph x11'
+		subprocess.call(command12, shell=True)
+
+	if S: 
+		command13='pepinfo {0}_esearch.seq.fa  -sprotein1 -graph ps'
+		subprocess.call(command13, shell=True)
+
+if aa_plot=="NO":
+	print("Exiting programe")
+	sys.exit()
